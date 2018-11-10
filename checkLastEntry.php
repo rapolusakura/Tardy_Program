371c2e04ab609bca8b61ac960eb94472f71a0b99
Sakura Rapolu <rapolusakura@gmail.com>
2018-11-09 17:54:35 -0800

    can check date of last record

---
+++ checkLastEntry.php
@@ -0,0 +1,24 @@
+<?php
+	require 'connect.php';
+	date_default_timezone_set("America/Los_Angeles");
+  //gets specific spreadsheet ID for the school
+
+  $result = $conn->query("
+    SELECT * FROM `late` ORDER BY time DESC LIMIT 1
+  ");
+
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			//gets date of last entry in yyyy-mm-dd
+      $dayOfLastEntry = explode(" ", $row["time"])[0];
+      $currentDate = date("Y-m-d");
+      if ($dayOfLastEntry == $currentDate) {
+        echo "spreadsheet is up to date!";
+      }
+      else {
+        echo "spreadsheet needs to be changed";
+      }
+		}
+	}
+	mysqli_close($conn);
+?>
