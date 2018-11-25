bc5366f20779959ec1509fc31bf35a602981010a
Sakura Rapolu <rapolusakura@gmail.com>
2018-11-24 17:21:41 -0800

    changed dropdown to autocomplete

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Tardy Registration Login</title>
+<link rel="stylesheet" href="index.css">
+<link rel="shortcut icon" href="wildcat.ico"/>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="w3.css">
+<link rel="stylesheet" href="google.css">
+<link rel="stylesheet" href="font.css">
+<link rel="stylesheet" href="login.css">
+</head>
+<body>
+  <br><br>
+  <div class="autocomplete" style="width:300px;">
+  <input id="myInput" type="text" placeholder="Enter school name">
+</div>
+    <br><br>
+		<input style  = "padding: 16px" autofocus type="text" id = "username" autocomplete = "off" placeholder="ex. Enter username"></input>
+    <input style  = "padding: 16px" autofocus type="password" id = "password" autocomplete = "off" placeholder="ex. Enter password"></input>
+    <button onclick="login()">Login</button>
+    <br><br>
+    <h5 id="loginStatus"></h5>
+<script type="text/javascript" src="login.js"></script>
+<div style = "padding: 30px; margin-top: 12em;" class = nonprint><h5> Created by Sakura Rapolu C/O 2018</h5></div>
+</body>
+</html>
