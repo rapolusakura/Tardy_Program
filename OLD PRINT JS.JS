d8bb5275c37a405ceed474806da43bde388b3773
Sakura Rapolu <rapolusakura@gmail.com>
2018-12-19 12:44:04 -0800

    updated random stuff

--- OLD PRINT JS.JS
+++ OLD PRINT JS.JS
@@ -4,10 +4,10 @@ function signIn() {
   });
   var name = $('input#name').val();
   if ($.trim(name) != '') {
-    $.post('print.php', {
-      name: name
-    }, function(data) {
-      $.post('spreadsheetIsCurrent.php', {}, function(isCurrent) {
+    $.post('spreadsheetIsCurrent.php', {}, function(isCurrent) {
+      $.post('print.php', {
+        name: name
+      }, function(data) {
         if (isCurrent == "false") {
           $.post("quickstart.php", {}, function(spreadsheetID) {
             setUpSheets(spreadsheetID, true);
@@ -19,18 +19,18 @@ function signIn() {
             $('#response').html('<p>Spreadsheet was last updated on ' + getCurrentDate() + '.</p>');
           });
         }
+        var parsedData = JSON.parse(data)
+        $('div#name-data').text(parsedData[0]);
+        document.getElementsByName('name')[0].value = "";
+        //prints slip
+        var myWindow = window.open("", "Tardy Slip");
+        myWindow.document.write(parsedData[1]);
+        myWindow.print();
+        myWindow.close();
+        setTimeout(function() {
+          document.getElementById("name").focus();
+        }, 1);
       });
-      //prints slip
-      var parsedData = JSON.parse(data)
-      $('div#name-data').text(parsedData[0]);
-      document.getElementsByName('name')[0].value = "";
-      var myWindow = window.open("", "Tardy Slip");
-      myWindow.document.write(parsedData[1]);
-      myWindow.print();
-      myWindow.close();
-      setTimeout(function() {
-        document.getElementById("name").focus();
-      }, 1);
     });
   }
 }
--- setCookie.php
+++ setCookie.php
@@ -1,3 +1,5 @@
 <?php
 setcookie("school_id", $_POST['school_id'], time() + (86400 * 30), "/"); // 86400 = 1 day
+// $domain = ($_SERVER['HTTP_HOST'] != 'localhost') ? $_SERVER['HTTP_HOST'] : false;
+// setcookie("school_id", $_POST['school_id'], time() + (86400 * 30), '/', $domain, false);
 ?>
