d4cdf4c33e231e7da6a8ed55a2ca3c68cd076c54
Sakura Rapolu <rapolusakura@gmail.com>
2019-03-14 13:52:49 -0700

    idk

--- print.php
+++ print.php
@@ -25,6 +25,7 @@
 			  SET `info`.`sheets-format` = '" . $txt."'
 			  WHERE `info`.`school-id` = '" . $_COOKIE["school_id"]."'
 			");
+			
 
 			//write to textfile for offline access
 			$fileName = 'Late_Student_Records/'.date('m-d-Y').'.txt';
